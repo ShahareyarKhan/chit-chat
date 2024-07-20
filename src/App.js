959b8a679bda9b7d4b37ef0efa487a4d2363227b
ShahareyarKhan <shahareyar2003@gmail.com>
2024-07-21 00:17:21 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,36 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
+import './App.css';
 import Home from './components/Home';
-
 import Login from './Authentication/Login';
 import Register from './Authentication/Register';
 import Forgot_Password from './Authentication/Forgot_Password';
 import ChatSection from './components/ChatSection';
 import ChatAI from './components/ChatAI';
-
-import socketIO from 'socket.io-client';
 import Profile from './components/Profile';
 import VideoCall from './components/VideoCall';
-const socket = socketIO.connect('https://chit-chat-api-lilac.vercel.app');
 
+import socketIO from 'socket.io-client';
+
+const socket = socketIO.connect('https://chit-chat-api-lilac.vercel.app');
 
 const App = () => {
   return (
-    <div className='w-full '>
+    <div className='w-full'>
       <Router>
-        
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
-          <Route path='/chat/:id' element={<ChatSection socket={socket}/>} />
+          <Route path='/chat/:id' element={<ChatSection socket={socket} />} />
           <Route path='/api-auth-login-signup-forgot-password' element={<Forgot_Password />} />
           <Route path='/ai-chat' element={<ChatAI />} />
-          <Route path='/profile' element={<Profile/>} />
-          <Route path='/video-call' element={<VideoCall/>} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/video-call' element={<VideoCall />} />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
