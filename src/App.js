5dcbd1eb04044a4d384882eaef01ca1f72b7b55f
ShahareyarKhan <shahareyar2003@gmail.com>
2024-08-13 15:22:16 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const socketUrl = window.location.hostname === 'localhost'
   ? 'http://localhost:5000'
   : 'https://chit-chat-api-lilac.vercel.app';
 
-const socket = io('https://chit-chat-api-lilac.vercel.app', {
+const socket = io('wss://chit-chat-api-lilac.vercel.app', {
   transports: ['websocket'] // No need for secure: true
 });
 
